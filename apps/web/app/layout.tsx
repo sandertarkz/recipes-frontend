4ce24d7217ee53cc7293d60155bbff289038f12f
sandertar <sander.taranov@gmail.com>
2025-02-02 19:53:35 +0100

    Add basic routes

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-
+import Link from "next/link";
 import "@workspace/ui/globals.css";
 import { Providers } from "@/components/providers";
 
@@ -23,6 +23,24 @@ export default function RootLayout({
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
+        <header>
+          <nav className="flex justify-center w-full p-4">
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/recipes">Recipes</Link>
+              </li>
+              <li>
+                <Link href="/register">Register</Link>
+              </li>
+              <li>
+                <Link href="/login">Login</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <Providers>{children}</Providers>
       </body>
     </html>
