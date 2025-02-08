0eabac3dd682b032cebcf6deb541f97fc68c193a
sandertar <sander.taranov@gmail.com>
2025-02-08 19:18:20 +0100

    Set default light theme

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -22,7 +22,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       disableTransitionOnChange
       enableColorScheme
